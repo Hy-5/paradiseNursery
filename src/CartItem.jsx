a22cafeb9ba48e8df0a4aa6ffbc0e0e88d2f9646
Ismael Coulibaly <coulbaly.ismael@gmail.com>
2024-10-15 00:03:12 -0400

    inc/dec-rement implementation

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -3,13 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
 
+
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
   };
 
   const handleContinueShopping = (e) => {
@@ -22,13 +22,17 @@ const CartItem = ({ onContinueShopping }) => {
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name:item.name, quantity:item.quantity+1}));
   };
 
   const handleDecrement = (item) => {
-   
+   if (item.quantity>1){
+    dispatch(updateQuantity({name:item.name, quantity:item.quantity-1}));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
